81f12bde79540a01cc82a7bc46c2bf224401d1ba
Tad Okazaki <Tad@Tads-MacBook-Air.local>
2018-11-05 14:23:15 -0800

    These are changed words

--- recur.cc
+++ recur.cc
@@ -1,19 +1,26 @@
 #include <iostream>
 
 //TODO #1.1: add a function prototype for fib
+int fib(int);
 
 //TODO #2.1: add a function prototype for fact
 
 int main()
 {
 	//TODO: #1.2 call your fib function, print the result
-	
+	fib(5);
+
 	//TODO: #2.2 call your fact function, print the result
 
 	return 0;
 }
 
 //TODO #2.3: add your recursive fib function from class
+int fib(int n)
+{
 
+
+return n;
+}
 
 //TODO #2.4: add your recursive fact function from class
